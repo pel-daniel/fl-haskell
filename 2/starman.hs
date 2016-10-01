5b820ca0e7f66de8a784efa683de011d7535ee67
Daniel Garcia <pel_daniel@hotmail.com>
2016-10-01 03:41:06 -0500

    2.11 Copy descriptions of each function from official repo

--- 2/starman.hs
+++ 2/starman.hs
@@ -1,10 +1,12 @@
 check :: String -> String -> Char -> (Bool, String)
+-- check whether a single char is in the mystery word
 check word display c
   = (c `elem` word, [if x == c
           then c
           else y | (x, y) <- zip word display])
 
 turn :: String -> String -> Int -> IO ()
+-- single turn for user
 turn word display n =
   do if n == 0
        then putStrLn "You lose"
@@ -13,6 +15,7 @@ turn word display n =
               else mkguess word display n
 
 mkguess :: String -> String -> Int -> IO ()
+-- user inputs a single char (first on the line)
 mkguess word display n =
   do putStrLn (display ++ "  " ++ take n (repeat '*'))
      putStr "  Enter your guess: "
@@ -22,4 +25,5 @@ mkguess word display n =
      turn word display' n'
 
 starman :: String -> Int -> IO ()
+-- top-level function. Usage: starman "WORD" NUM_TURNS
 starman word n = turn word ['-' | x <- word] n
